9bb8d85257e695f2a99bc9f1ba92a98da0e037fb
tjrwls0826 <tjrwls0826@naver.com>
2024-02-08 22:18:44 +0900

    configure response and result

--- src/main/java/com/example/convpay_practice/dto/PayResponse.java
+++ src/main/java/com/example/convpay_practice/dto/PayResponse.java
@@ -1,4 +1,30 @@
 package com.example.convpay_practice.dto;
 
 public class PayResponse {
+    //결제 결과
+    PayResult payResult;
+
+    //결제 성공 금액
+    Integer paidAmount;
+
+    public PayResponse(PayResult payResult, Integer paidAmount) {
+        this.payResult = payResult;
+        this.paidAmount = paidAmount;
+    }
+
+    public PayResult getPayResult() {
+        return payResult;
+    }
+
+    public void setPayResult(PayResult payResult) {
+        this.payResult = payResult;
+    }
+
+    public Integer getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(Integer paidAmount) {
+        this.paidAmount = paidAmount;
+    }
 }
---
+++ src/main/java/com/example/convpay_practice/dto/PayResult.java
@@ -0,0 +1,6 @@
+package com.example.convpay_practice.dto;
+
+public enum PayResult {
+    SUCCESS,
+    FAIL
+}
